2807ce1517903f29679c1bfcc264ffa4d28023c7
ANI1KET <aniketrouniyar12@gmail.com>
2025-02-25 16:37:16 +0530

    Made Changes in GraphQL Connection

--- app/dashboard/graphQL/graphQLClient.ts
+++ app/dashboard/graphQL/graphQLClient.ts
@@ -1,7 +1,23 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+// import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+// export const apolloclient = new ApolloClient({
+//   credentials: 'include',
+//   cache: new InMemoryCache(),
+//   uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER_BASE_URL,
+// });
+
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const apolloclient = new ApolloClient({
-  credentials: 'include',
+  link: new HttpLink({
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER_BASE_URL,
+    credentials: 'include',
+    fetch: (url, options) => {
+      return fetch(url, {
+        ...options,
+        credentials: 'include',
+      });
+    },
+  }),
   cache: new InMemoryCache(),
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER_BASE_URL,
 });
