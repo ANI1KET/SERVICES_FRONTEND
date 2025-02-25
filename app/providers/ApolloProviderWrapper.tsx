496524937666fe86363a4bf483aaf6c97b5864da
ANI1KET <aniketrouniyar12@gmail.com>
2025-02-26 00:43:23 +0530

    Fixed Bug

--- app/providers/ApolloProviderWrapper.tsx
+++ app/providers/ApolloProviderWrapper.tsx
@@ -1,13 +1,46 @@
+// 'use client';
+
+// import { ApolloProvider } from '@apollo/client';
+
+// import { apolloclient } from '../dashboard/graphQL/graphQLClient';
+
+// export default function ApolloProviderWrapper({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return <ApolloProvider client={apolloclient}>{children}</ApolloProvider>;
+// }
+
 'use client';
 
-import { ApolloProvider } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  NormalizedCacheObject,
+} from '@apollo/client';
+import { useState, useEffect } from 'react';
 
-import { apolloclient } from '../dashboard/graphQL/graphQLClient';
+import createApolloClient from '../dashboard/graphQL/graphQLClient';
 
 export default function ApolloProviderWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <ApolloProvider client={apolloclient}>{children}</ApolloProvider>;
+  const [client, setClient] =
+    useState<ApolloClient<NormalizedCacheObject> | null>(null);
+
+  useEffect(() => {
+    const setupApollo = async () => {
+      const apolloClient = await createApolloClient();
+      setClient(apolloClient);
+    };
+
+    setupApollo();
+  }, []);
+
+  if (!client) return <p>Loading...</p>;
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
