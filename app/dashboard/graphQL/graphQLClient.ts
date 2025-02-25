856772ce6be696e531ab8f349f4bfd2cd9eb5f34
ANI1KET <aniketrouniyar12@gmail.com>
2025-02-25 19:28:19 +0530

    Testing

--- app/dashboard/graphQL/graphQLClient.ts
+++ app/dashboard/graphQL/graphQLClient.ts
@@ -9,15 +9,10 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const apolloclient = new ApolloClient({
+  credentials: 'include',
+  cache: new InMemoryCache(),
   link: new HttpLink({
     uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER_BASE_URL,
     credentials: 'include',
-    fetch: (url, options) => {
-      return fetch(url, {
-        ...options,
-        credentials: 'include',
-      });
-    },
   }),
-  cache: new InMemoryCache(),
 });
