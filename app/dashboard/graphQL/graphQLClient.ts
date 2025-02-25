d18ee5319e7b22786775fa300562a520f542f152
ANI1KET <aniketrouniyar12@gmail.com>
2025-02-25 21:05:17 +0530

    Testing

--- app/dashboard/graphQL/graphQLClient.ts
+++ app/dashboard/graphQL/graphQLClient.ts
@@ -1,18 +1,7 @@
-// import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-// export const apolloclient = new ApolloClient({
-//   credentials: 'include',
-//   cache: new InMemoryCache(),
-//   uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER_BASE_URL,
-// });
-
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const apolloclient = new ApolloClient({
   credentials: 'include',
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER_BASE_URL,
-    credentials: 'include',
-  }),
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER,
 });
