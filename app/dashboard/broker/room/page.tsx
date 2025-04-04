5597153902e9db32ee00e3eb88a522943b6103af
ANI1KET <aniketrouniyar12@gmail.com>
2025-04-04 23:13:47 +0530

    Deploy

--- app/dashboard/broker/room/page.tsx
+++ app/dashboard/broker/room/page.tsx
@@ -4,7 +4,7 @@ import { useQuery } from '@apollo/client';
 import { useSession } from 'next-auth/react';
 
 import { LIMIT } from '@/app/lib/reusableConst';
-import { RoomWithMediaUrl } from '@/app/types/types';
+import { NewListedRoom } from '@/app/types/types';
 import { GET_LISTED_ROOMS } from '../../graphQL/userQuery';
 import MainLayout from '../../components/broker/room/MainLayout';
 import SearchLayout from '../../components/broker/room/SearchLayout';
@@ -13,7 +13,7 @@ const BrokerRoomDashboard = () => {
   const session = useSession();
 
   const { data, loading, error, fetchMore } = useQuery<{
-    user: { rooms: RoomWithMediaUrl[] };
+    user: { rooms: NewListedRoom[] };
   }>(GET_LISTED_ROOMS, {
     // fetchPolicy: 'cache-and-network',
     variables: { id: session.data?.user.userId, offset: 0, limit: LIMIT },
